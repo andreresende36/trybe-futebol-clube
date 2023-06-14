0a34c736b93a8f4771d0644fdc5b5aa2464348f1
André Resende <andreresende36@gmail.com>
2023-06-14 19:12:11 -0300

    Docker, db, front and back containers OK

---
+++ app/backend/src/database/migrations/01-create-teams-table.ts
@@ -0,0 +1,23 @@
+import { Model, QueryInterface, DataTypes } from "sequelize";
+import ITeam from "../../Interfaces/ITeam";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        field:'team_name',
+        allowNull: false
+      }
+    })
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('Teams');
+  },
+};
---
+++ app/backend/src/database/migrations/02-create-matches-table.ts
@@ -0,0 +1,51 @@
+import { Model, QueryInterface, DataTypes } from "sequelize";
+import IMatch from "../../Interfaces/IMatch";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IMatch>>('matches', {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      homeTeamId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        field: 'home_team_id',
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      homeTeamGoals: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        field: 'home_team_goals',
+        allowNull: false
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        field: 'away_team_id',
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+      awayTeamGoals: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        field: 'away_team_goals',
+        allowNull: false
+      },
+      inProgress: {
+        type: DataTypes.BOOLEAN,
+        field: 'in_progress',
+        allowNull: false
+      }
+    })
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('matches');
+  },
+};
